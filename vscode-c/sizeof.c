9173c4b322c2ee6dd98eb66cae3e624cc59143e2
Re.op <1985307086@qq.com>
2023-02-14 23:04:31 +0800

    v1v

---
+++ vscode-c/sizeof.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+
+    int i = 123;
+    char a = -1;
+    printf("i = %f\n",(double)i);
+    printf("%f\n",12.0/0.0);
+    printf("sizeof()= %d\n", sizeof(short));
+    printf("123\b\n123\n");
+}
