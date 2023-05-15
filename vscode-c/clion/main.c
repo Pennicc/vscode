8e0ba357e3fa6f53e9e592085c5856c540b42a9d
Re.op <1985307086@qq.com>
2023-05-15 23:15:47 +0800

    v1

--- vscode-c/clion/main.c
+++ vscode-c/clion/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 int main(){
+    int target = 3, result;
 
+    int a=1, b=1 , c;
+    for(int i=2 ; i < target ; ++i){
+        c = a+b;
+        a = b;
+        b = c;
+    }
+    result = c;
+    printf("%d" , result);
     return 0;
 }
 
